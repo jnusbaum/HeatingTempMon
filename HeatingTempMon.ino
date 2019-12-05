cc99ea782c6fc81c8c96604dcb95e450a4a2a279
Jim Nusbaum <jim.nusbaum@vitalityindex.com>
2019-12-05 15:27:06 -0700

    changed delay code, debug print

--- HeatingTempMon.ino
+++ HeatingTempMon.ino
@@ -16,6 +16,16 @@
 
 ESP8266WiFiMulti WiFiMulti;
 
+#ifdef DEBUG
+ #define DEBUG_PRINT(x)     Serial.print (x)
+ #define DEBUG_PRINTF(x, y)     Serial.printf (x, y)
+ #define DEBUG_PRINTLN(x)  Serial.println (x)
+#else
+ #define DEBUG_PRINT(x)
+ #define DEBUG_PRINTF(x, y)
+ #define DEBUG_PRINTLN(x)
+#endif
+
 // Setup 3 oneWire instances to communicate with temp sensors
 OneWire oneWireUpstairs(0);
 OneWire oneWireDownstairs(2);
@@ -27,7 +37,10 @@ DallasTemperature sensorsDownstairs(&oneWireDownstairs);
 DallasTemperature sensorsBoilerAndValve(&oneWireBoilerAndValve);
 
 
+#ifdef DEBUG
+
 // function to print a device address
+// only called when DEBUG
 void printAddress(DeviceAddress deviceAddress)
 {
   for (uint8_t i = 0; i < 8; i++)
@@ -39,6 +52,7 @@ void printAddress(DeviceAddress deviceAddress)
 
 
 // function to print the temperature for a device
+// only called when DEBUG
 void printTemperature(DeviceAddress d, float tempC, float tempF)
 {
   Serial.print("Temp for Address: ");
@@ -51,35 +65,43 @@ void printTemperature(DeviceAddress d, float tempC, float tempF)
   Serial.println(tempF);
 }
 
+#endif
+
 
 void setupDevice(DallasTemperature sensors, DeviceAddress device)
 {
+  #ifdef DEBUG
   	Serial.print("Device Address: ");
   	printAddress(device);
   	Serial.println();
+  #endif
   
   	// set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
   	sensors.setResolution(device, 9);
-  
+   
+  #ifdef DEBUG
   	Serial.print("Device Resolution: ");
   	Serial.print(sensors.getResolution(device), DEC);
   	Serial.println();
+  #endif
 }
 
 
 void setup() {
 
-  Serial.begin(115200);
-
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
+  #ifdef DEBUG
+    Serial.begin(115200);
+  
+    Serial.println();
+    Serial.println();
+    Serial.println();
+  
+    for (uint8_t t = 4; t > 0; t--) {
+      DEBUG_PRINTF("[SETUP] WAIT %d...\n", t);
+      Serial.flush();
+      delay(1000);
+    }
+  #endif
 
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP("nusbaum-24g", "we live in Park City now");
@@ -112,12 +134,14 @@ void processTemp(DallasTemperature sensors, devinfo &d)
     // It responds almost immediately. Let's print out the data
     float tempC = sensors.getTempC(d.devaddr);
     float tempF = sensors.toFahrenheit(tempC);
-    printTemperature(d.devaddr, tempC, tempF); // Use a simple function to print out the data
-    Serial.print("[HTTP] begin...\n");
+    #ifdef DEBUG
+      printTemperature(d.devaddr, tempC, tempF); // Use a simple function to print out the data
+    #endif
+    DEBUG_PRINTLN("[HTTP] begin...");
     if (http.begin(client, "http://192.168.0.104:8000/api/heating/sensors/" + d.devname + "/data")) 
     {  
       // HTTP
-      Serial.print("[HTTP] POST...\n");
+      DEBUG_PRINTLN("[HTTP] POST...");
       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
       String data = String("value-real=") + String(tempF, 2);
       // start connection and send HTTP header
@@ -127,53 +151,61 @@ void processTemp(DallasTemperature sensors, devinfo &d)
       if (httpCode > 0)
       {
         // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+        DEBUG_PRINTF("[HTTP] POST... code: %d\n", httpCode);
 
         // file found at server
         if (httpCode != HTTP_CODE_CREATED) 
         {
-          Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+          DEBUG_PRINTF("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
         }
       } 
       else 
       {
-        Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        DEBUG_PRINTF("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
       }
 
       http.end();
     } 
     else 
     {
-      Serial.printf("[HTTP} Unable to connect\n");
+      DEBUG_PRINTLN("[HTTP} Unable to connect");
     }
 }
 
-    
-void loop() {
-  // wait for WiFi connection
-  if ((WiFiMulti.run() == WL_CONNECTED)) 
-  {
-    // call sensors.requestTemperatures() to issue a global temperature 
-    // request to all devices on the bus
-    Serial.print("Requesting temperatures...");
-    sensorsUpstairs.requestTemperatures(); // Send the command to get temperatures
-    sensorsDownstairs.requestTemperatures(); // Send the command to get temperatures
-    sensorsBoilerAndValve.requestTemperatures(); // Send the command to get temperatures
-    Serial.println("Done");
 
-    for (int x = 0; x < 6; ++x)
+int period = 10000;
+unsigned long previousMillis = 0;
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if(currentMillis - previousMillis > period) 
+  {
+    previousMillis = currentMillis;
+      
+    // wait for WiFi connection
+    if ((WiFiMulti.run() == WL_CONNECTED)) 
     {
-      processTemp(sensorsUpstairs, devicesUpstairs[x]);
-    }
-    for (int x = 0; x < 8; ++x)
-    {
-      processTemp(sensorsDownstairs, devicesDownstairs[x]);
-    }
-    for (int x = 0; x < 4; ++x)
-    {
-      processTemp(sensorsBoilerAndValve, devicesBoilerAndValve[x]);
+      // call sensors.requestTemperatures() to issue a global temperature 
+      // request to all devices on the bus
+      DEBUG_PRINT("Requesting temperatures...");
+      sensorsUpstairs.requestTemperatures(); // Send the command to get temperatures
+      sensorsDownstairs.requestTemperatures(); // Send the command to get temperatures
+      sensorsBoilerAndValve.requestTemperatures(); // Send the command to get temperatures
+      DEBUG_PRINTLN("Done");
+  
+      for (int x = 0; x < 6; ++x)
+      {
+        processTemp(sensorsUpstairs, devicesUpstairs[x]);
+      }
+      for (int x = 0; x < 8; ++x)
+      {
+        processTemp(sensorsDownstairs, devicesDownstairs[x]);
+      }
+      for (int x = 0; x < 4; ++x)
+      {
+        processTemp(sensorsBoilerAndValve, devicesBoilerAndValve[x]);
+      }
     }
   }
-
-  delay(10000);
 }
