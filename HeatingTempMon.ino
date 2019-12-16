23a3245778bd05e3096199cf49faea81433bbd3f
Jim Nusbaum <jim.nusbaum@vitalityindex.com>
2019-12-16 15:11:11 -0700

    dev names

--- HeatingTempMon.ino
+++ HeatingTempMon.ino
@@ -95,12 +95,12 @@ sensorbus *busses[] = { &busUpstairs, &busDownstairs, &busBoilerAndValve };
 
 #ifdef CLOSET
 
-devinfo office_in = { "OFFICE-IN", &dev_nineteen }; // 19
-devinfo mbr_in = { "MBR-IN", &dev_twenty }; // 20
-devinfo mbath_in = { "MBATH-IN", &dev_twentyone }; // 21
-devinfo office_out = { "OFFICE-OUT", &dev_twentythree }; // 23
-devinfo mbr_out = { "MBR-OUT", &dev_twentytwo }; // 22
-devinfo mbath_out = { "MBATH-OUT", &dev_twentyfour }; // 24
+devinfo office_in = { "OFFICE-IN", &dev_twentyfour }; // 24
+devinfo mbr_in = { "MBR-IN", &dev_twentytwo }; // 22
+devinfo mbath_in = { "MBATH-IN", &dev_twentythree }; // 23
+devinfo office_out = { "OFFICE-OUT", &dev_twentyone }; // 21
+devinfo mbr_out = { "MBR-OUT", &dev_twenty }; // 20
+devinfo mbath_out = { "MBATH-OUT", &dev_nineteen }; // 19
 
 devinfo *devicesCloset[] = { &office_in, &mbr_in, &mbath_in, &office_out, &mbr_out, &mbath_out };
 OneWire oneWireCloset(2);
