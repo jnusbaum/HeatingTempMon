77ee73348df2349e84293f53c87d3ec05ad5a5b8
Jim Nusbaum <jim.nusbaum@vitalityindex.com>
2019-12-15 17:35:30 -0700

    cleanup

--- HeatingTempMon.ino
+++ HeatingTempMon.ino
@@ -29,6 +29,85 @@
 #define DEBUG_PRINTLN(x)
 #endif
 
+// device addresses
+typedef struct {
+  String devname;
+  DeviceAddress *devaddr;
+}
+devinfo;
+
+// temp sensor buss
+typedef struct {
+  int pin;
+  DallasTemperature *bus;
+  int numsensors;
+  devinfo *sensors;
+}
+sensorbus;
+
+
+#define MECHROOM
+
+// Set up onewire and sensors
+#ifdef MECHROOM
+
+devinfo laundry_in = { "LAUNDRY-IN", &dev_one }; // 1
+devinfo kitchen_in = { "KITCHEN-IN", &dev_two }; // 2
+devinfo garage_in = { "GARAGE-IN", &dev_three }; // 3
+devinfo kitchen_out = { "KITCHEN-OUT", &dev_four }; // 4
+devinfo garage_out = { "GARAGE-OUT", &dev_five }; // 5
+devinfo laundry_out = { "LAUNDRY-OUT", &dev_six }; // 6
+devinfo valve_out = { "VALVE-OUT", &dev_seven }; // 7
+devinfo valve_insys = { "VALVE-INSYS", &dev_eight }; // 8
+devinfo boiler_out = { "BOILER-OUT", &dev_nine }; // 9
+devinfo boiler_in = { "BOILER-IN", &dev_ten }; // 10
+devinfo family_out = { "FAMILY-OUT", &dev_eleven }; // 11
+devinfo office_out = { "OFFICE-OUT", &dev_twelve }; // 12
+devinfo exercise_out = { "EXERCISE-OUT", &dev_thirteen }; // 13
+devinfo guest_out = { "GUEST-OUT", &dev_fourteen }; // 14
+devinfo family_in = { "FAMILY-IN", &dev_fifteen }; // 15
+devinfo office_in = { "OFFICE-IN", &dev_sixteen }; // 16
+devinfo exercise_in = { "EXERCISE-IN", &dev_seventeen }; // 17
+devinfo guest_in = { "GUEST-IN", &dev_eighteen }; // 18
+
+devinfo devicesUpstairs[] = { kitchen_out, laundry_out, garage_out, laundry_in, garage_in, kitchen_in };
+OneWire oneWireUpstairs(0);
+DallasTemperature sensorsUpstairs(&oneWireUpstairs);
+sensorbus busUpstairs = { 0, &sensorsUpstairs, 6, devicesUpstairs };
+
+devinfo devicesDownstairs[] = { family_out, office_out, exercise_out, guest_out, family_in, office_in, exercise_in, guest_in };
+OneWire oneWireDownstairs(2);
+DallasTemperature sensorsDownstairs(&oneWireDownstairs);
+sensorbus busDownstairs = { 2, &sensorsDownstairs, 8, devicesDownstairs };
+
+devinfo devicesBoilerAndValve[] = { boiler_in, boiler_out, valve_insys, valve_out };
+OneWire oneWireBoilerAndValve(4);
+DallasTemperature sensorsBoilerAndValve(&oneWireBoilerAndValve);
+sensorbus busBoilerAndValve = ( 4, &sensorsBoilerAndValve, 4, devicesBoilerAndValve };
+
+numbusses = 3;
+sensorbus busses[] = { busUpstairs, busDownstairs, busBoilerAndValve };
+
+#endif
+
+
+
+#ifdef CLOSET
+
+devinfo nineteen = { "NINETEEN", dev_nineteen }; // 19
+devinfo twenty = { "TWENTY", dev_twenty }; // 20
+devinfo twentyone = { "TWENTYONE", dev_twentyone }; // 21
+devinfo twentytwo = { "TWENTYTWO", dev_twentytwo }; // 22
+devinfo twentythree = { "TWENTYTHREE", dev_twentythree }; // 23
+devinfo twentyfour = { "TWENTYFOUR", dev_twentyfour }; // 24
+devinfo twentyfive = { "TWENTYFIVE", dev_twentyfive }; // 25
+
+devinfo devicesUpstairs[] = { kitchen_out, laundry_out, garage_out, laundry_in, garage_in, kitchen_in };
+devinfo devicesDownstairs[] = { family_out, office_out, exercise_out, guest_out, family_in, office_in, exercise_in, guest_in };
+devinfo devicesBoilerAndValve[] = { boiler_in, boiler_out, valve_insys, valve_out };
+
+numsensors = 3;
+DallasTemperature tsensors[3];
 // Setup 3 oneWire instances to communicate with temp sensors
 OneWire oneWireUpstairs(0);
 OneWire oneWireDownstairs(2);
@@ -38,6 +117,9 @@ OneWire oneWireBoilerAndValve(4);
 DallasTemperature sensorsUpstairs(&oneWireUpstairs);
 DallasTemperature sensorsDownstairs(&oneWireDownstairs);
 DallasTemperature sensorsBoilerAndValve(&oneWireBoilerAndValve);
+
+#endif
+
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "us.pool.ntp.org", -25200, 5000000);
@@ -73,20 +155,22 @@ void printTemperature(DeviceAddress d, float tempC, float tempF)
 #endif
 
 
-void setupDevice(DallasTemperature sensors, DeviceAddress device)
+void setupDevices(sensorbus &bus)
 {
+  for (int y = 0; y < bus.numsensors; ++y)
+  {
 #ifdef DEBUG
   Serial.print("Device Address: ");
-  printAddress(device);
+  printAddress(bus.sensors[y].devaddr);
   Serial.println();
 #endif
 
   // set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
-  sensors.setResolution(device, 9);
+  sensors->setResolution(bus.sensors[y].devaddr, 9);
 
 #ifdef DEBUG
   Serial.print("Device Resolution: ");
-  Serial.print(sensors.getResolution(device), DEC);
+  Serial.print(sensors->getResolution(bus.sensors[y].devaddr), DEC);
   Serial.println();
 #endif
 }
@@ -109,86 +193,77 @@ void setup() {
 #endif
 
   WiFi.begin("nusbaum-24g", "we live in Park City now");
-  
-  sensorsUpstairs.begin();
-  sensorsDownstairs.begin();
-  sensorsBoilerAndValve.begin();
 
-  // setup devices
-  for (int x = 0; x < 6; ++x)
+  for (int x = 0; x < numbusses; ++x)
   {
-    setupDevice(sensorsUpstairs, devicesUpstairs[x].devaddr);
+    busses[x].bus->begin();
+    setupDevices(busses[x]);
   }
-  for (int x = 0; x < 8; ++x)
-  {
-    setupDevice(sensorsDownstairs, devicesDownstairs[x].devaddr);
-  }
-  for (int x = 0; x < 4; ++x)
-  {
-    setupDevice(sensorsBoilerAndValve, devicesBoilerAndValve[x].devaddr);
-  }
-
+    
   DEBUG_PRINT("Wait for WiFi... ");
-
+  
   while (WiFi.status() != WL_CONNECTED) {
     DEBUG_PRINT(".");
     delay(500);
   }
-
+  
   DEBUG_PRINTLN("");
   DEBUG_PRINTLN("WiFi connected");
   DEBUG_PRINTLN("IP address: ");
   DEBUG_PRINTLN(WiFi.localIP());
-
+  
   timeClient.begin();
   timeClient.forceUpdate();
 }
 
 
-void processTemp(String tstr, DallasTemperature sensors, devinfo &d)
+void processTemps(sensorbus &bus)
 {
   WiFiClient client;
   HTTPClient http;
 
-  float tempC = sensors.getTempC(d.devaddr);
-  float tempF = sensors.toFahrenheit(tempC);
-#ifdef DEBUG
-  printTemperature(d.devaddr, tempC, tempF); // Use a simple function to print out the data
-#endif
-  DEBUG_PRINTLN("[HTTP] begin...");
-  String url = String("http://") + HOST + ":" + PORT + "/sensors/" + d.devname + "/data";
-  DEBUG_PRINTF("[HTTP] POSTing to %s\n", url.c_str());
-  if (http.begin(client, url))
+  for (y = 0; y < bus.numsensors; ++y)
   {
-    // HTTP
-    DEBUG_PRINTLN("[HTTP] POST...");
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    String data = String("value-real=") + String(tempF, 2) + String("&") + String("timestamp=") + tstr;
-    // start connection and send HTTP header
-    int httpCode = http.POST(data);
-
-    // httpCode will be negative on error
-    if (httpCode > 0)
+    float tempC = bus.bus->getTempC(bus.sensors[y].devaddr);
+    float tempF = bus.bus->toFahrenheit(tempC);
+  #ifdef DEBUG
+    printTemperature(bus.sensors[y].devaddr, tempC, tempF); // Use a simple function to print out the data
+  #endif
+    DEBUG_PRINTLN("[HTTP] begin...");
+    String url = String("http://") + HOST + ":" + PORT + "/sensors/" + bus.sensors[y].devname + "/data";
+    DEBUG_PRINTF("[HTTP] POSTing to %s\n", url.c_str());
+    if (http.begin(client, url))
     {
-      // HTTP header has been send and Server response header has been handled
-      DEBUG_PRINTF("[HTTP] POST... code: %d\n", httpCode);
-
-      // file found at server
-      if (httpCode != HTTP_CODE_CREATED)
+      // HTTP
+      DEBUG_PRINTLN("[HTTP] POST...");
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      String data = String("value-real=") + String(tempF, 2) + String("&") + String("timestamp=") + tstr;
+      // start connection and send HTTP header
+      int httpCode = http.POST(data);
+  
+      // httpCode will be negative on error
+      if (httpCode > 0)
+      {
+        // HTTP header has been send and Server response header has been handled
+        DEBUG_PRINTF("[HTTP] POST... code: %d\n", httpCode);
+  
+        // file found at server
+        if (httpCode != HTTP_CODE_CREATED)
+        {
+          DEBUG_PRINTF("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+      }
+      else
       {
         DEBUG_PRINTF("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
       }
+  
+      http.end();
     }
     else
     {
-      DEBUG_PRINTF("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      DEBUG_PRINTLN("[HTTP} Unable to connect");
     }
-
-    http.end();
-  }
-  else
-  {
-    DEBUG_PRINTLN("[HTTP} Unable to connect");
   }
 }
 
@@ -206,27 +281,20 @@ void loop()
 
     // call sensors.requestTemperatures() to issue a global temperature
     unsigned long etime = timeClient.getEpochTime();
+    for (int x = 0; x < numbusses; ++x)
+    {
+      busses[x].bus->requestTemperatures();
+    }
     DEBUG_PRINTF("\n\ntimestamp = %lu\n\n", etime);
-    sensorsUpstairs.requestTemperatures(); // Send the command to get temperatures
-    sensorsDownstairs.requestTemperatures(); // Send the command to get temperatures
-    sensorsBoilerAndValve.requestTemperatures(); // Send the command to get temperatures
-
+    
     DateTime ldtm(etime);
     char buffer[] = "YYYY-MM-DD-hh-mm-ss";
     ldtm.toString(buffer);
     DEBUG_PRINTF("\n\ntimestr = %s\n\n", buffer);
 
-    for (int x = 0; x < 6; ++x)
+    for (int x = 0; x < numbusses; ++x)
     {
-      processTemp(buffer, sensorsUpstairs, devicesUpstairs[x]);
-    }
-    for (int x = 0; x < 8; ++x)
-    {
-      processTemp(buffer, sensorsDownstairs, devicesDownstairs[x]);
-    }
-    for (int x = 0; x < 4; ++x)
-    {
-      processTemp(buffer, sensorsBoilerAndValve, devicesBoilerAndValve[x]);
+      processTemps(busses[x]);
     }
   }
 }
